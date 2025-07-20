5788964ea60e375fc3750f5c7b17b17b71f7eeea
Jacob Tartabini <139934788+jacobtartabini@users.noreply.github.com>
2025-07-20 09:45:56 -0400

    Create middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const isLoggedIn = request.cookies.has('next-auth.session-token');
+
+  if (!isLoggedIn && request.nextUrl.pathname !== '/Login') {
+    return NextResponse.redirect(new URL('/Login', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!_next|favicon.ico).*)'],
+};
